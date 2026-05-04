dc1eadd1fd4de91fa8c0a9deacdc405a523fb3fc
TheAmazingGssi <105499786+TheAmazingGssi@users.noreply.github.com>
2026-05-04 21:00:21 +0300

    inventory start

--- Assets/_Scripts/Player/Inventory.cs
+++ Assets/_Scripts/Player/Inventory.cs
@@ -1,11 +1,13 @@
 using System.Collections.Generic;
+using System.Text.Json;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    const string INVENTORY_PATH = "/fishInventory.json";
     public static Inventory Instance;
 
-    public List<FishStats> Fish { get; private set; } = new List<FishStats>();
+    public Dictionary<string, int> dict = new Dictionary<string, int>();
 
     private void Awake()
     {
@@ -16,23 +18,50 @@ public class Inventory : MonoBehaviour
         }
         Instance = this;
         DontDestroyOnLoad(gameObject);
+        LoadState();
     }
 
-    public void AddFish(FishStats fishStats)
+    public void AddFish(FishStats fishStats, int amount)
     {
-        Fish.Add(fishStats);
+        if(dict.ContainsKey(fishStats.ID))
+            dict[fishStats.ID] += amount;
+        else
+            dict.Add(fishStats.ID, amount);
+        SaveState();
     }
-
-    public void RemoveFish(FishStats fishStats)
+    public void AddFish(FishStats fishStats) => AddFish(fishStats, 1);
+    public void RemoveFish(FishStats fishStats, int amount)
     {
-        Fish.Remove(fishStats);
+        if (dict.ContainsKey(fishStats.ID) && dict[fishStats.ID] >= amount)
+            dict[fishStats.ID] -= amount;
+        else
+            Debug.LogError("Tried removing a fish the player did not have");
+        SaveState();
     }
-
+    public void RemoveFish(FishStats fishStats) => RemoveFish(fishStats, 1);
     public void ClearInventory()
     {
-        Fish.Clear();
+        dict.Clear();
+        SaveState();
     }
 
 
-
+    private void SaveState()
+    {
+        string jsonFile = "{\n";
+        foreach (KeyValuePair<string, int> kvp in dict)
+        {
+            jsonFile += $"\"{kvp.Key}\": {kvp.Value}\n";
+        }
+        jsonFile += "}";
+        string path = Application.persistentDataPath + INVENTORY_PATH;
+        System.IO.File.WriteAllText(path, jsonFile);
+        Debug.Log(jsonFile);
+    }
+    private void LoadState()
+    {
+        string jsonFile = System.IO.File.ReadAllText(Application.persistentDataPath + INVENTORY_PATH);
+        dict = JsonUtility.FromJson<Dictionary<string, int>>(jsonFile);
+        Debug.Log(jsonFile);
+    }
 }
