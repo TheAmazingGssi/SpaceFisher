a63a1765cfea8450951210efde9d0d68715ee0fd
TheAmazingGssi <105499786+TheAmazingGssi@users.noreply.github.com>
2026-05-04 18:40:08 +0300

    guid for fish

--- Assets/_Scripts/ScriptableObjects/FishStats.cs
+++ Assets/_Scripts/ScriptableObjects/FishStats.cs
@@ -1,14 +1,15 @@
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 [CreateAssetMenu(fileName = "FishStats", menuName = "Scriptable Objects/FishStats")]
 public class FishStats : ScriptableObject
-{
-    [field: SerializeField] public string Id { get; private set; }
+{    
     [SerializeField] private Sprite fishSprite;
 
     [Header("Minigame")]
     [SerializeField] private float mgSpeed;
-    //[SerializeField] private float height;
 
     [Header("Aquarium")]
     [Tooltip("X: Min Y: Max")]
@@ -17,22 +18,33 @@ public class FishStats : ScriptableObject
     [SerializeField] private float aquariumSpeed;
     [SerializeField][Range(0, 90)] private float verticalSwimming = 30;
 
-
-
-    private void OnValidate()
-    {
-        if (string.IsNullOrEmpty(Id))
-        {
-            Id = System.Guid.NewGuid().ToString();
-        }
-    }
-
     public float MGSpeed => mgSpeed;
-    //public float Height => height;
     public Vector2 SwimTime => swimTime;
     public float PauseTime => pauseTime;
     public float AquariumSpeed => aquariumSpeed;
     public float VerticalSwimming => verticalSwimming;
     public Sprite FishSprite => fishSprite;
-    public string ID { get; private set; } = System.Guid.NewGuid().ToString();
+
+    [SerializeField]
+    private string id;
+
+    public string ID => id;
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        string path = AssetDatabase.GetAssetPath(this);
+
+        if (!string.IsNullOrEmpty(path))
+        {
+            string guid = AssetDatabase.AssetPathToGUID(path);
+
+            if (id != guid)
+            {
+                id = guid;
+                EditorUtility.SetDirty(this);
+            }
+        }
+    }
+#endif
 }
