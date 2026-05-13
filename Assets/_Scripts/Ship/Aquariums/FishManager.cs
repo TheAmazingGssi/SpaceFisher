507f634fd1dafaf969478b2011f0c731451a1829
Maayana24 <maayan2412001@gmail.com>
2026-05-13 20:45:07 +0300

    Fish fix

--- Assets/_Scripts/Ship/Aquariums/FishManager.cs
+++ Assets/_Scripts/Ship/Aquariums/FishManager.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class FishManager : MonoBehaviour
 {
     [field: SerializeField] public SpriteRenderer SpriteRenderer {  get; private set; }
-    public FishStats Stats {  get; private set; }
+    [field: SerializeField] public FishStats Stats {  get; private set; }
 
     [SerializeField] private CapsuleCollider2D fishCollider;
 
