52e1f0351907caa443984e03383c8de14771f55c
Maayana24 <maayan2412001@gmail.com>
2026-05-16 00:40:05 +0300

    hold fingre

--- Assets/_Scripts/UI/ClickbleObject.cs
+++ Assets/_Scripts/UI/ClickbleObject.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem.EnhancedTouch;
 using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
@@ -5,6 +6,10 @@ using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 public abstract class ClickableObject : MonoBehaviour
 {
     [SerializeField] protected Collider2D _collider;
+
+    private float holdTime = 0.5f;
+    private Coroutine holdCoroutine;
+    private bool fingerDownOnObject;
 
     private void Awake()
     {
@@ -17,26 +22,54 @@ public abstract class ClickableObject : MonoBehaviour
         Touch.onFingerUp += HandleFingerUp;
     }
 
+    private void OnDisable()
+    {
+        Touch.onFingerDown -= HandleFingerDown;
+        Touch.onFingerUp -= HandleFingerUp;
+        CancelHold();
+    }
+
     private void HandleFingerDown(Finger finger)
     {
         Vector2 worldPos = Camera.main.ScreenToWorldPoint(finger.screenPosition);
         if (_collider.OverlapPoint(worldPos))
+        {
+            fingerDownOnObject = true;
             OnFingerDown();
+            holdCoroutine = StartCoroutine(HoldRoutine());
+        }
     }
 
     private void HandleFingerUp(Finger finger)
     {
         Vector2 worldPos = Camera.main.ScreenToWorldPoint(finger.screenPosition);
-        if (_collider.OverlapPoint(worldPos))
-            OnFingerUp();
+
+        CancelHold();
+
+        if (fingerDownOnObject)
+        {
+            fingerDownOnObject = false;
+            if (_collider.OverlapPoint(worldPos))
+                OnFingerUp();
+        }
+    }
+
+    private IEnumerator HoldRoutine()
+    {
+        yield return new WaitForSeconds(holdTime);
+        OnFingerHold();
+    }
+
+    private void CancelHold()
+    {
+        if (holdCoroutine != null)
+        {
+            StopCoroutine(holdCoroutine);
+            holdCoroutine = null;
+        }
     }
 
     protected virtual void OnFingerDown() { }
     protected virtual void OnFingerUp() { }
-
-    private void OnDisable()
-    {
-        Touch.onFingerDown -= HandleFingerDown;
-        Touch.onFingerUp -= HandleFingerUp;
-    }
+    protected virtual void OnFingerHold() { }
 }
