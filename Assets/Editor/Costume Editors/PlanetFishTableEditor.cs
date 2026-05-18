d6de2bbfa2fe9b38fd3b4e06a9f4db9065844ad3
TheAmazingGssi <105499786+TheAmazingGssi@users.noreply.github.com>
2026-05-18 19:51:06 +0300

    dialouge window start

--- Assets/Editor/Costume Editors/PlanetFishTableEditor.cs
+++ Assets/Editor/Costume Editors/PlanetFishTableEditor.cs
@@ -17,6 +17,15 @@ public class PlanetFishTableEditor : Editor
     }
     public override void OnInspectorGUI()
     {
-        base.OnInspectorGUI();
+        base.OnInspectorGUI(); //Most of the code appears on FishSpawnDataDrawer
+        if (GUILayout.Button("Auto Import Fish"))
+            AutoImportFish();
+    }
+    private void AutoImportFish()
+    {
+        if (!EditorUtility.DisplayDialog("Are You Sure About That", "This action will create a new array with all the fish that signed " + (target as PlanetFishTable).Planet + " as their planet, with default values for everything else.", "I know what Im doing", "Wait I dont want the current values to be erased"))
+            return;
+        
+
     }
 }
