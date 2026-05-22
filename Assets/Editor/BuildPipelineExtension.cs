788c00cf7a02b0bb37b95052753d4cf1089da6fb
TheAmazingGssi <105499786+TheAmazingGssi@users.noreply.github.com>
2026-05-22 18:11:44 +0300

    refactor fix

--- Assets/Editor/BuildPipelineExtension.cs
+++ Assets/Editor/BuildPipelineExtension.cs
@@ -17,7 +17,7 @@ public class BuildPipelineExtension
             if (EditorUtility.DisplayDialog("Scene mismatch!", "Choose fix to only have scenes that exist in the Constant class", "Fix & Continue", "Continue without fixing"))
                 options.scenes = constantScenes.Select(name => AssetDatabase.FindAssets($"{name} t:Scene").Select(AssetDatabase.GUIDToAssetPath).FirstOrDefault(p => System.IO.Path.GetFileNameWithoutExtension(p) == name)).Where(p => p != null).ToArray();
 
-        var fishWithoutSprite = FishTypeList.Instance.list.Values.Where(f => f.FishSprite == null).ToList();
+        var fishWithoutSprite = ScriptablesDatabase.Instance.fishList.Values.Where(f => f.FishSprite == null).ToList();
         if (fishWithoutSprite.Count > 0)
             if (!EditorUtility.DisplayDialog("Missing fish sprites!", "Some of the fish have no sprite. Do you wish to continue?", "Continue Anyway", "Cancel Build"))
                 return;
