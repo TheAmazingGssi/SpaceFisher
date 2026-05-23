ec54662d586bbdd7769d908d3d3c40f3c3776610
TheAmazingGssi <105499786+TheAmazingGssi@users.noreply.github.com>
2026-05-23 20:34:50 +0300

    Crosswalk + Art

---
+++ Assets/_Scripts/Ship/Placement System/Crosswalk.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+[ExecuteAlways]
+public class Crosswalk : MonoBehaviour
+{
+    [SerializeField] int crosswalkWidth;
+    [SerializeField] SpriteRenderer sprite;
+    [SerializeField] BoxCollider2D col;
+    const float gridSizeX = 4;
+    const float gridSizeY = 3;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sprite.size = new Vector2(crosswalkWidth * gridSizeX, gridSizeY);
+        col.size = sprite.size;
+        if(crosswalkWidth%2  != 0 )
+        {
+            sprite.transform.localPosition = new Vector3(gridSizeX/2, gridSizeY/2, 0);
+        }
+        else
+            sprite.transform.localPosition = new Vector3(0, gridSizeY/2, 0); ;
+    }
+}
