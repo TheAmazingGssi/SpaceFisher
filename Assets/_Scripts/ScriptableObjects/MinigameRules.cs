10d9790bccf1ba254200a489864e750731011b96
TheAmazingGssi <105499786+TheAmazingGssi@users.noreply.github.com>
2026-07-02 14:13:08 +0300

    removed temp files

--- Assets/_Scripts/ScriptableObjects/MinigameRules.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "MinigameRules", menuName = "Scriptable Objects/MinigameRules")]
-public class MinigameRules : ScriptableObject
-{
-    [field: SerializeField] public float SpawnDistanceMin { get; private set; }
-    [field: SerializeField] public float SpawnDistanceMax { get; private set; }
-    public float RandomSpawnDistance { get => Random.Range(SpawnDistanceMin, SpawnDistanceMax); }
-}
