147ce9addbbd4dcdf54cc49c8fbe23040438d48e
Maayana24 <maayan2412001@gmail.com>
2026-07-06 18:27:43 +0300

    New ground

---
+++ Assets/_Scripts/UI/Ship/Buttons/UpgradeButton.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeButton : ItemButton<KeyValuePair<Upgrade, int>>
+{
+    public override void Setup(KeyValuePair<Upgrade, int> data)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
