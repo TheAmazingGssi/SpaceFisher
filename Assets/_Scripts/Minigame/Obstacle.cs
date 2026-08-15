71a9744e8dcd35584403ab0593f6ca58e94bbeda
TheAmazingGssi <105499786+TheAmazingGssi@users.noreply.github.com>
2026-08-15 17:50:14 +0300

    mines explode

--- Assets/_Scripts/Minigame/Obstacle.cs
+++ Assets/_Scripts/Minigame/Obstacle.cs
@@ -2,12 +2,19 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+    [SerializeField] private Animator anim;
+    const string BOOM_TRIGGER = "Boom";
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == Constants.Tags.Player)
         {
             MinigameManager.Instance.Phase = MinigamePhase.Up;
+            anim.SetTrigger(BOOM_TRIGGER);
             Bus<FishFell>.Raise(new FishFell());
         }
+    }   
+    public void DeleteSelf()
+    {
+        Destroy(gameObject);
     }
 }
