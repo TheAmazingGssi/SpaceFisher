8f59a7a9d2a9a18a3ca8c88359384777d75ba90a
TheAmazingGssi <105499786+TheAmazingGssi@users.noreply.github.com>
2026-08-17 00:37:53 +0300

    bubbles work

--- Assets/_Scripts/Minigame/Bubble.cs
+++ Assets/_Scripts/Minigame/Bubble.cs
@@ -3,12 +3,30 @@ using UnityEngine;
 public class Bubble : MonoBehaviour
 {
     Vector2 mainPosition;
-    [SerializeField] float wiggleModifier;
-    [SerializeField] float wiggleSpeed;
-    [SerializeField] Camera cam;
+    [SerializeField] Vector2 wiggleModifierMaxMin;
+    [SerializeField] Vector2 wiggleSpeedMaxMin;
     [SerializeField] Vector2 scaleMaxMin;
-    const float buffer = 1;
+    [SerializeField] Vector2 verticalSpeedMaxMin;
+    [SerializeField] Sprite[] sprites;
 
+    [SerializeField] Camera cam;
+    [SerializeField] SpriteRenderer spriteRenderer;
+    const float buffer = 1;
+    float currentSpeed;
+    float wiggleModifier;
+    float wiggleSpeed;
+
+    private void OnEnable()
+    {
+        Bus<MinigameStart>.OnEvent += OnMinigameStart;
+        Bus<MinigameEnd>.OnEvent += OnMinigameEnd;
+    }
+    private void OnDisable()
+    {
+        Bus<MinigameStart>.OnEvent -= OnMinigameStart;
+        Bus<MinigameEnd>.OnEvent -= OnMinigameEnd;
+    }
+    
     private void Start()
     {
         mainPosition = transform.position;
@@ -17,12 +35,27 @@ public class Bubble : MonoBehaviour
     }
     private void Update()
     {
+        mainPosition.y += currentSpeed * Time.deltaTime;
         transform.position = new Vector2(mainPosition.x + (wiggleModifier * Mathf.Sin(Time.time * wiggleSpeed)), mainPosition.y);
-        //OnBecameInvisible();
     }
 
     private void OnBecameInvisible()
     {
+        Teleport();
+    }
+    private void OnMinigameStart(MinigameStart e)
+    {
+        MinigameManager.Instance.Phase = MinigamePhase.Down;
+        spriteRenderer.enabled = true;
+        Teleport();
+    }
+    private void OnMinigameEnd(MinigameEnd e)
+    {
+        spriteRenderer.enabled = false;
+    }
+    public void Teleport()
+    {
+        Debug.Log("Im teleporting");
         Vector2 checkVect;
         float addSign;
         if (MinigameManager.Instance.Phase == MinigamePhase.Down)
@@ -35,11 +68,13 @@ public class Bubble : MonoBehaviour
             checkVect = Vector2.one;
             addSign = buffer;
         }
-        
+
         Vector2 camEdge = cam.ViewportToWorldPoint(checkVect);
         float newY = camEdge.y + addSign;
-        if (newY >= -10)
-            return;
+        if (newY >= -20)
+        {
+            newY = -20;
+        }
 
         camEdge.x *= Mathf.Sign(camEdge.x);
         float newX = Random.Range(-camEdge.x, camEdge.x);
@@ -47,6 +82,13 @@ public class Bubble : MonoBehaviour
 
         float scale = Random.Range(scaleMaxMin.x, scaleMaxMin.y);
         transform.localScale = new Vector3(scale, scale, 1);
+
+        int index = Random.Range(0, sprites.Length);
+        spriteRenderer.sprite = sprites[index];
+
+        currentSpeed = Random.Range(verticalSpeedMaxMin.x, verticalSpeedMaxMin.y);
+        wiggleModifier = Random.Range(wiggleModifierMaxMin.x, wiggleModifierMaxMin.y);
+        wiggleSpeed = Random.Range(wiggleSpeedMaxMin.x, wiggleSpeedMaxMin.y);
 
     }
 }
